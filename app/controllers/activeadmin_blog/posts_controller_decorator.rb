72d60d033da8534757ef87a843b4d42029a61e26
Alex Kravets <santyor@gmail.com>
2012-09-13 14:34:02 +0300

    restrict access to blog show page; changed footer;

--- app/controllers/activeadmin_blog/posts_controller_decorator.rb
+++ app/controllers/activeadmin_blog/posts_controller_decorator.rb
@@ -1,5 +1,5 @@
 ActiveadminBlog::PostsController.class_eval do
-  #before_filter :authenticate_user!
+  before_filter :authenticate_user!, only: [:show]
   before_filter :set_navigation_label
 
   def set_navigation_label
