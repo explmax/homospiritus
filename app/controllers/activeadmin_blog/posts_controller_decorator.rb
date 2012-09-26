cb18ecd960ce4c4787b44d474726eb708217e838
Alex Kravets <santyor@gmail.com>
2012-09-26 22:40:25 +0300

    update blog;

--- app/controllers/activeadmin_blog/posts_controller_decorator.rb
+++ app/controllers/activeadmin_blog/posts_controller_decorator.rb
@@ -1,8 +1,16 @@
 ActiveadminBlog::PostsController.class_eval do
   before_filter :authenticate_user!, only: [:show]
-  before_filter :set_navigation_label
 
-  def set_navigation_label
-    @nav = "blog"
+  def search
+    @search_query  = params[:q]
+    @posts = ActiveadminBlog::BlogPost.blog_search(@search_query)
+    @posts = @posts.page params[:page]
+  end
+
+  def category
+    category_slug = params[:slug]
+    @category = ActiveadminBlog::BlogCategory.where(slug: category_slug).first()
+    @posts = ActiveadminBlog::BlogPost.published_in_category(category_slug)
+    @posts = @posts.page params[:page]
   end
 end
