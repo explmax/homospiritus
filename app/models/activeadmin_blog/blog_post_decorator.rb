f6607d98416dce8b3dbf3e5a20050f4eb28c6302
Alex Kravets <santyor@gmail.com>
2012-09-26 22:50:57 +0300

    fix excerpt for post;

---
+++ app/models/activeadmin_blog/blog_post_decorator.rb
@@ -0,0 +1,13 @@
+ActiveadminBlog::BlogPost.class_eval do
+  def excerpt
+    html = Nokogiri::HTML(content)
+    begin
+      ps = html.css('p').select{|p| not p.content.empty? }
+      p1 = ps[0].content
+      p2 = ps[1].content
+      "<p>#{p1}</p><p>#{p2}</p>"
+    rescue
+      ""
+    end
+  end
+end
